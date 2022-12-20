87569dfe67e13f5b429012c9db0351451b85edf3
Chima Ihueze <drkingsleyc@gmail.com>
2022-12-20 12:48:14 +0100

    added getters and setters

--- models/Item.java
+++ models/Item.java
@@ -15,5 +15,23 @@ public Item(Item source) {
     this.name = source.name;
     this.price = source.price;
 }
+
+//  getters
+public String getName() {
+    return name;
+}
+
+public double getPrice() {
+    return price;
+}
+
+// setters
+public void setName(String name) {
+    this.name = name;
+}
+
+public void setPrice(double price) {
+    this.price = price;
+}
   
 }

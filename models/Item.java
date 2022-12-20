a87b8253c06a78594dd6f7471f78e1fe028e88d3
Chima Ihueze <drkingsleyc@gmail.com>
2022-12-20 12:23:01 +0100

    added the Item() constructor

--- models/Item.java
+++ models/Item.java
@@ -3,5 +3,11 @@ package models;
 public class Item {
     private String name;
     private double price;
-
+    
+// constructor
+public Item(String name, double price) {
+    this.name = name;
+    this.price = price;
+}
+  
 }
